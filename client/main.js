b0c5cd21a4078f6fdef4b7fa4cdad777250c93ae
GYUMI-ANG <54223902+ANK-UMI@users.noreply.github.com>
2021-05-24 13:16:20 +0900

    Update main.js

--- client/main.js
+++ client/main.js
@@ -12,7 +12,7 @@ init()
 
 function init() {
 scene = new THREE.Scene()
-scene.background = new THREE.Color(0xa0a0a0)
+scene.background = new THREE.Color(0xffffff)
 
 camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000)
 camera.position.set(0, 50, 100)
@@ -23,8 +23,8 @@ let light2 = new THREE.DirectionalLight(0xffffff)
 light2.position.set(0, -50, 50)
 scene.add(light2)
 
-let grid = new THREE.GridHelper(1200, 60)
-scene.add(grid)
+//let grid = new THREE.GridHelper(1200, 60)
+//scene.add(grid)
 
 renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
@@ -37,16 +37,17 @@ function render() {
     socket.on('datos-giro', function (datos) {
         document.getElementById('datos').innerHTML = datos
 
-        angulos = datos.split(' ')
+         angulos = datos.split(' ')
+        //angulos = datos.replace("ypr","ypr ").split(" ")
 
         x = parseInt(angulos[1], 10)
         y = parseInt(angulos[2], 10)
         z = parseInt(angulos[3], 10)
 
-       // mesh.rotation.set(x * Math.PI / 180, y * Math.PI / 180, z * Math.PI / 180)
-        
-       // mesh.rotation.set(x * Math.PI / 180, y * Math.PI / 180, z * Math.PI / 180)
-       // mesh.rotation.set( -z * Math.PI / 180, -x  * Math.PI / 180, -y * Math.PI / 180)
+        mesh.rotation.set(x * Math.PI / 180, y * Math.PI / 180, z * Math.PI / 180)
+
+     // document.getElementById('info').innerHTML = "X: " + x + " Y: " + y + " Z: " + z;
+     // mesh.rotation.set(- z * Math.PI / 180, - x * Math.PI / 180, - y * Math.PI / 180)
 
     })
 
